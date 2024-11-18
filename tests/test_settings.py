4cf38bc4a636459d93c737f342e1496ccc566b0c
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2024-11-18 15:47:49 +0100

    v1.0.0

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -24,4 +24,13 @@ class RemovedSettingsCheckTests(SimpleTestCase):
     def test_ignoring_specific_warning_works(self):
         all_issues = checks.run_checks(tags=None)
 
-        self.assertEqual(len(all_issues), 0)
+        self.assertNotIn(
+            checks.Warning(
+                "The 'TRANSACTIONS_MANAGED' setting was removed in Django 1.4 and its use is not recommended.",
+                hint="Please refer to the documentation: https://docs.djangoproject.com/en/stable/releases/"
+                "1.4/#features-removed-in-1-4.",
+                obj="TRANSACTIONS_MANAGED",
+                id="removals.W014/transactions_managed",
+            ),
+            all_issues,
+        )
