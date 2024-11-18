5147d2bfa61bdf71949413333518c81b0fd99774
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2024-11-18 15:38:39 +0100

    v1.0.0

--- django_removals/checks/settings.py
+++ django_removals/checks/settings.py
@@ -1,97 +1,83 @@
 from django.conf import settings
 from django.core.checks import Warning
 
-# TODO: create tags dynamically with removals.5.1.my_var?
 REMOVED_SETTINGS = {
-    # Django 1.0
-    # https://docs.djangoproject.com/en/stable/releases/1.0/
-    # Django 1.1
-    # https://docs.djangoproject.com/en/stable/releases/1.1/#features-deprecated-in-1-1
-    # Django 1.2
-    # https://docs.djangoproject.com/en/stable/releases/1.2/#features-deprecated-in-1-2
-    "DATABASE_ENGINE",
-    "DATABASE_HOST",
-    "DATABASE_NAME",
-    "DATABASE_OPTIONS",
-    "DATABASE_PASSWORD",
-    "DATABASE_PORT",
-    "DATABASE_USER",
-    "TEST_DATABASE_CHARSET",
-    "TEST_DATABASE_COLLATION",
-    "TEST_DATABASE_NAME",
-    # Django 1.3
-    # https://docs.djangoproject.com/en/stable/releases/1.3/#features-deprecated-in-1-3
-    # Django 1.4
-    # https://docs.djangoproject.com/en/stable/releases/1.4/#features-deprecated-in-1-4
-    "TRANSACTIONS_MANAGED",
-    # Django 1.5
-    # https://docs.djangoproject.com/en/stable/releases/1.5/#features-deprecated-in-1-5
-    "AUTH_PROFILE_MODULE",
-    # Django 1.7
-    # https://docs.djangoproject.com/en/stable/releases/1.7/#features-removed-in-1-7
-    "SOUTH_DATABASE_ADAPTER",
-    "SOUTH_DATABASE_ADAPTERS",
-    "SOUTH_AUTO_FREEZE_APP",
-    "SOUTH_TESTS_MIGRATE",
-    "SOUTH_LOGGING_ON",
-    "SOUTH_LOGGING_FILE",
-    "SOUTH_MIGRATION_MODULES",
-    "SOUTH_USE_PYC",
-    "TEST_CREATE",
-    "TEST_USER_CREATE",
-    "TEST_PASSWD",
-    "TEST_DATABASE_ENGINE",
-    "TEST_DATABASE_HOST",
-    "TEST_DATABASE_OPTIONS",
-    "TEST_DATABASE_PASSWORD",
-    "TEST_DATABASE_PORT",
-    "TEST_DATABASE_USER",
-    # Django 1.8
-    # https://docs.djangoproject.com/en/stable/releases/1.8/#features-removed-in-1-8
-    "SEND_BROKEN_LINK_EMAILS",
-    "CACHE_MIDDLEWARE_ANONYMOUS_ONLY",
-    # Django 1.9
-    # https://docs.djangoproject.com/en/stable/releases/1.9/#features-removed-in-1-9
-    # Django 1.10
-    # https://docs.djangoproject.com/en/stable/releases/1.10/#features-removed-in-1-10
-    "ALLOWED_INCLUDE_ROOTS",
-    "LOGOUT_URL",
-    "TEMPLATE_CONTEXT_PROCESSORS",
-    "TEMPLATE_DEBUG",
-    "TEMPLATE_DIRS",
-    "TEMPLATE_LOADERS",
-    "TEMPLATE_STRING_IF_INVALID",
-    # Django 2.0
-    # https://docs.djangoproject.com/en/stable/releases/2.0/#features-removed-in-2-0
-    "MIDDLEWARE_CLASSES",
-    # Django 2.1
-    # https://docs.djangoproject.com/en/stable/releases/2.1/#features-removed-in-2-1
-    "USE_ETAGS",
-    "SECURE_BROWSER_XSS_FILTER",
-    # Django 3.0
-    # https://docs.djangoproject.com/en/stable/releases/3.0/#features-removed-in-3-0
-    "DEFAULT_CONTENT_TYPE",
-    "PASSWORD_RESET_TIMEOUT_DAYS",
-    # Django 3.1
-    # https://docs.djangoproject.com/en/stable/releases/3.1/#features-removed-in-3-1
-    "DEFAULT_FILE_STORAGE",
-    "FILE_CHARSET",
-    # Django 4.0
-    # https://docs.djangoproject.com/en/stable/releases/4.0/#features-removed-in-4-0
-    "DEFAULT_HASHING_ALGORITHM",
-    # Django 4.1
-    # https://docs.djangoproject.com/en/stable/releases/4.1/#features-removed-in-4-1
-    # Django 5.0
-    # https://docs.djangoproject.com/en/stable/releases/5.0/#features-removed-in-5-0
-    "USE_L10N",
-    "USE_DEPRECATED_PYTZ",
-    "CSRF_COOKIE_MASKED",
-    # Django 5.1
-    # https://docs.djangoproject.com/en/stable/releases/5.1/#features-removed-in-5-1
-    "STATICFILES_STORAGE",
-    # Django 6.0
-    # RemovedInDjango60Warning: when the deprecation ends, replace with:
-    # "FORMS_URLFIELD_ASSUME_HTTPS",
+    "1.2": {
+        "DATABASE_ENGINE",
+        "DATABASE_HOST",
+        "DATABASE_NAME",
+        "DATABASE_OPTIONS",
+        "DATABASE_PASSWORD",
+        "DATABASE_PORT",
+        "DATABASE_USER",
+        "TEST_DATABASE_CHARSET",
+        "TEST_DATABASE_COLLATION",
+        "TEST_DATABASE_NAME",
+    },
+    "1.4": {
+        "TRANSACTIONS_MANAGED",
+    },
+    "1.5": {
+        "AUTH_PROFILE_MODULE",
+    },
+    "1.7": {
+        "SOUTH_DATABASE_ADAPTER",
+        "SOUTH_DATABASE_ADAPTERS",
+        "SOUTH_AUTO_FREEZE_APP",
+        "SOUTH_TESTS_MIGRATE",
+        "SOUTH_LOGGING_ON",
+        "SOUTH_LOGGING_FILE",
+        "SOUTH_MIGRATION_MODULES",
+        "SOUTH_USE_PYC",
+        "TEST_CREATE",
+        "TEST_USER_CREATE",
+        "TEST_PASSWD",
+        "TEST_DATABASE_ENGINE",
+        "TEST_DATABASE_HOST",
+        "TEST_DATABASE_OPTIONS",
+        "TEST_DATABASE_PASSWORD",
+        "TEST_DATABASE_PORT",
+        "TEST_DATABASE_USER",
+    },
+    "1.8": {
+        "SEND_BROKEN_LINK_EMAILS",
+        "CACHE_MIDDLEWARE_ANONYMOUS_ONLY",
+    },
+    "1.10": {
+        "ALLOWED_INCLUDE_ROOTS",
+        "LOGOUT_URL",
+        "TEMPLATE_CONTEXT_PROCESSORS",
+        "TEMPLATE_DEBUG",
+        "TEMPLATE_DIRS",
+        "TEMPLATE_LOADERS",
+        "TEMPLATE_STRING_IF_INVALID",
+    },
+    "2.0": {
+        "MIDDLEWARE_CLASSES",
+    },
+    "2.1": {
+        "USE_ETAGS",
+        "SECURE_BROWSER_XSS_FILTER",
+    },
+    "3.0": {
+        "DEFAULT_CONTENT_TYPE",
+        "PASSWORD_RESET_TIMEOUT_DAYS",
+    },
+    "3.1": {
+        "DEFAULT_FILE_STORAGE",
+        "FILE_CHARSET",
+    },
+    "4.0": {
+        "DEFAULT_HASHING_ALGORITHM",
+    },
+    "5.0": {
+        "USE_L10N",
+        "USE_DEPRECATED_PYTZ",
+        "CSRF_COOKIE_MASKED",
+    },
+    "5.1": {
+        "STATICFILES_STORAGE",
+    },
 }
 
 
@@ -102,14 +88,19 @@ def check_removed_settings(**kwargs):
 
     warnings = []
     for setting_name in dir(settings):
-        if setting_name.isupper() and setting_name in REMOVED_SETTINGS:
-            warnings.append(  # noqa: PERF401
-                Warning(
-                    f"The {setting_name!r} setting was removed and its use is " f"not recommended.",
-                    hint="Please refer to the documentation and remove/replace " "this setting.",
-                    obj=setting_name,
-                    id="settings.W001",
+        for django_version, removed_settings in REMOVED_SETTINGS.items():
+            if setting_name.isupper() and setting_name in removed_settings:
+                warning_id = f"W0{django_version.replace(".", "")}/{setting_name.lower()}"
+                warnings.append(
+                    Warning(
+                        f"The {setting_name!r} setting was removed in Django {django_version} and its use is not "
+                        f"recommended.",
+                        hint=f"Please refer to the documentation: "
+                        f"https://docs.djangoproject.com/en/stable/releases/{django_version}/"
+                        f"#features-removed-in-{django_version.replace('.', '-')}.",
+                        obj=setting_name,
+                        id=f"removals.{warning_id}",
+                    )
                 )
-            )
 
     return warnings
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,3 @@
-from django.apps import apps
 from django.core import checks
 from django.test import SimpleTestCase, override_settings
 
@@ -6,16 +5,23 @@ from django.test import SimpleTestCase, override_settings
 class RemovedSettingsCheckTests(SimpleTestCase):
     @override_settings(TRANSACTIONS_MANAGED=True)
     def test_check_removed_settings(self):
-        all_issues = checks.run_checks(app_configs=apps.get_app_configs())
+        all_issues = checks.run_checks(tags=None)
 
         self.assertGreater(len(all_issues), 0)
 
         self.assertIn(
             checks.Warning(
-                "The 'TRANSACTIONS_MANAGED' setting was removed and its use " "is not recommended.",
-                hint="Please refer to the documentation and remove/replace " "this setting.",
+                "The 'TRANSACTIONS_MANAGED' setting was removed in Django 1.4 and its use is not recommended.",
+                hint="Please refer to the documentation: https://docs.djangoproject.com/en/stable/releases/"
+                "1.4/#features-removed-in-1-4.",
                 obj="TRANSACTIONS_MANAGED",
-                id="settings.W001",
+                id="removals.W014/transactions_managed",
             ),
             all_issues,
         )
+
+    @override_settings(SILENCED_SYSTEM_CHECKS=("removals.W014/transactions_managed",))
+    def test_ignoring_specific_warning_works(self):
+        all_issues = checks.run_checks(tags=None)
+
+        self.assertEqual(len(all_issues), 0)
